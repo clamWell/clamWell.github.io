0418ea8264f1279f3635f2c970c17829756ac1df
h62446244! <pepesi258@hanmail.net>
2020-06-08 18:18:26 +0900

    [github blog] * portfolio 정보 입력 중. 뷰단 이미지 추가.

--- _pages/js/portItemList.js
+++ _pages/js/portItemList.js
@@ -1,77 +1,77 @@
 var portItemList = [
   {
     "id": "0",
-    "slider-type": "0",
-    "title": "반지하실태보고서",
+    "type": "0",
+    "title": "반지하 실태 보고서",
     "date": "2020.04",
     "thumb": "port-thumb-1.jpg",
     "bigimage": "port_v_1_1.jpg, port_v_1_2.jpg, port_v_1_3.jpg",
     "link": "http://news.khan.co.kr/kh_storytelling/2020/underground/index.html",
     "workField": "기사작성 및 자료 조사(50%), 그래픽 제작(100%), 페이지 개발(100%)",
     "tag": "HTML, CSS, Javascript, D3.js, Data-mapping, R",
-    "description": "<p>기생충을 통해 주목을 받은 반지하의 국내 현황을 보도한 기사를 재가공해 유통한 데이터 분석 기사입니다. </p><p>단독으로 입수한 전국 반지하 현황 데이터를 동단위, 지역구 단위로 검색해 볼 수 있도록 했습니다. 전국 200여개의 시군구의 반지하 현황을 막대 그래프로 그려 수도권 집중도를 확인하고, 특히 반지하 거주 비율이 현저히 높은 서울시의 경우 서울시 지도에 시군구 별 다른 변수의 값을 통해 어떤 지역에 반지하 거주 비율이 높은지를 살펴보았습니다. </p><p>이전에 보도되지 못한 취재일지 및 현장 사진을 르포기사 형태로 재구성하였습니다.</p>",
-    "descriptionEng": "<p>Specialnews article pages projected with the Industrial & Economic Dept of Daily News Paper <KyungHyangShinMun>. An article is kind of report about people who live under the ground. Underground household are being recognized after the movie <Parasite> is on hit.</p><p>I draw colorized map and pole graph by processing some geographic and demographic data of household who live under the ground. Also user can search their administrative district or election precinct to know how many neighborhood are live under the ground.</p>",
+    "description": "<p>최초로 반지하의 국내 현황을 보도한 기사를 재가공해 유통한 데이터 분석 기사입니다. </p><p>단독으로 입수한 전국 반지하 현황 데이터를 동단위, 지역구 단위로 검색해 볼 수 있도록 했습니다. 전국 200여개의 시군구의 반지하 현황을 막대 그래프로 그려 수도권 집중도를 확인하고, 특히 반지하 거주 비율이 현저히 높은 서울시의 경우 서울시 지도에 시군구 별 다른 변수의 값을 통해 어떤 지역에 반지하 거주 비율이 높은지를 살펴보았습니다. </p><p>이전에 보도되지 못한 취재일지 및 현장 사진을 르포기사 형태로 재구성하였습니다.</p>",
+    "descriptionEng": "<p>A special news article page projected with the Industrial & Economic Dept of Daily News Paper <KyungHyangShinMun>. the article report the story about people who live under the ground. Underground household are being recognized after the movie <Parasite> is being hit.</p><p>I draw colorized map and pole graph by processing some geographic and demographic data of household who live under the ground. Also user can search their administrative district or election precinct to know how many neighborhood are live under the ground.</p>",
     "able": "true"
   },
   {
     "id": "1",
-    "slider-type": "0",
-    "title": "개인정보유출사건",
+    "type": "0",
+    "title": "도난당한 개인정보 칼이 되어 돌아오다",
     "date": "2020.03",
     "thumb": "port-thumb-11.jpg",
-    "bigimage": "port_v_2_1.jpg, port_v_2_2.jpg, port_v_2_3.jpg",
+    "bigimage": "port_v_2_1.jpg, port_v_2_2.jpg, port_v_2_3.jpg,port_v_2_4.jpg",
     "link": "http://el.multicampus.com/main/credu/user/course/zu_course_NR_M.jsp?p_subj=A55581#",
     "workField": "기사작성 및 데이터 조사·가공(80%), 그래픽 제작(100%), 페이지 개발(100%)",
     "tag": "HTML, CSS, Javascript, D3.js, archive",
-    "description": "<p>국내 대표 이러닝 업체 멀티캠퍼스와 협력해 진행한 개인정보보안 관련 교육콘텐츠 제작 프로젝트입니다. 교육콘텐츠 중 개인정보유출에 대한 경각심을 주는 유출사고 아카이브 제작을 담당했습니다. </p><p>2008년 이후 국내에서 발생한 개인정보 유출 사건을 취재 및 조사하여 데이터베이스로 만들었습니다. 이후 사건 개별을 규모, 유출원인 등 다양한 항목별로 조회해볼 수 있도록 아카이브화 하였습니다. 취합한 사건 데이터를 분석해 하단에 기사 형태로 소개하고 있습니다.</p>",
+    "description": "<p>국내 대표 이러닝 업체 멀티캠퍼스와 협력해 진행한 개인정보보안 관련 교육 콘텐츠물입니다. 영상, 게임형식 등 다양한 콘텐츠물을 시리즈로 제작했으며 이 중 개인정보 유출에 대한 경각심을 주는 개인정보 유출 아카이브 제작을 담당했습니다.</p><p>10년간 유출된 개인정보는 2억 3천만 건에 달합니다. 2000년대 이후 국내에서 발생한 개인정보 유출 사고들을 한자리에 모아봤습니다. 정보공개청구 및 언론에 보도 등을 토대로 유출 사건들을 정리했습니다.</p><p>이후 유출 사건들을 유출의 규모, 원인 등 다양한 조건에 따라 필터링하여 조회해볼 수 있도록 작업 하였습니다. 취합한 사건 데이터를 추가적으로 분석해 하단에 기사 형태로 소개하고 있습니다.</p>",
     "descriptionEng": "<p>Archiving project with the Multicampus(E-learning company). We gathered and refining 'personal Information extrusion cases' data during 2008-2019 in South Korea and visualized them.</p><p>As hooking, the danger of the crime by exploiting personal information is introduced in the beginning.</p>",
     "able": "true"
   },
   {
     "id": "2",
-    "slider-type": "0",
-    "title": "코로나",
+    "type": "0",
+    "title": "21세기 바이러스 총망라",
     "date": "2020.02",
     "thumb": "port-thumb-2.jpg",
-    "bigimage": null,
-    "link": null,
-    "workField": null,
-    "tag": null,
-    "description": null,
-    "descriptionEng": null,
+    "bigimage": "port_v_3_1.jpg, port_v_3_2.jpg, port_v_3_3.jpg",
+    "link": "http://news.khan.co.kr/kh_storytelling/2020/virus/index.html",
+    "workField": "데이터 조사·가공(50%), 페이지 개발(100%)",
+    "tag": "HTML, CSS, Javascript, leaflet.js",
+    "description": "<p>사스, 신종플루, 메르스, 코로나19 등 21세기 이후 전 세계를 흉흉하게 만든 주요 호흡기 전염병들의 확산·대응과정을 정리한 뉴스 서비스 페이지입니다. 코로나19 창궐 초기에 바이러스에 대한 정보를 제공하기 위한 목적으로 속성으로 제작되었으며, 기존의 다른 호흡기 전염병와 코로나19의 특징을 비교하는 것에 초점을 주었습니다.</p><p>이후 leaflet.js 라이브러리를 이용해 코로나19의 전세계 감염 현황을 지도위에 시각화했습니다. 질병관리본부가 제공하는 현황 정보를 크롤링해오는 방식으로 데이터를 실시간 업데이트 했으며, 마지막 데이터 업데이트 날짜는 4월 24일입니다.</p>",
+    "descriptionEng": "<p>A story news page project with the International Dept of Daily News Paper <KyungHyangShinMun>. We spread some confirm/death cases of the corona virus over the world map. An interesting story about the virus which are spreading worldwide in 21 century are also well organized.</p>",
     "able": "true"
   },
   {
     "id": "3",
-    "slider-type": "0",
-    "title": "특권, 차별",
+    "type": "0",
+    "title": "당신은 특권을 갖고 있나요, 차별을 받고 있나요",
     "date": "2020.01",
     "thumb": "port-thumb-3.jpg",
-    "bigimage": null,
-    "link": null,
-    "workField": null,
-    "tag": null,
-    "description": null,
-    "descriptionEng": null,
+    "bigimage": "port_v_4_1.jpg, port_v_4_2.jpg, port_v_4_3.jpg, port_v_4_4.jpg",
+    "link": "http://news.khan.co.kr/kh_storytelling/2020/minor/index.html",
+    "workField": "페이지 디자인·개발(100%)",
+    "tag": "HTML, CSS, Javascript, D3.js, GoogleSpreadSheets",
+    "description": "<p>사용자가 설문에 따라 자신의 특권적⇔차별적 정보를 입력하면, 응답 결과를 레이더 차트로 시각화해 보여주는 일종의 서비스 페이지입니다. 레이더 차트는 방향에 따라 위, 아래로 양면성을 갖고 있습니다. 우리는 이러한 레이더 차트를 통해 모든 개인이 어떤 면에서는 특권을 갖고 있더라도, 또 다른 면에서 차별받고 차별하는 존재임을 시각적으로 보여주고자 했습니다.</p><p>인터랙티브 차트는 D3.js로 구현했습니다. 추가적으로 구글 스프레드시트를 DB로 활용해 사용자의 응답 정보를 받아 기록하였고 이를 토대로 연령별, 혹은 성별에 따른 평균치를 사용자가 조회해볼 수 있도록 했습니다. 약 3천명의 사용자들의 설문에 참여했으며, 설문 결과 및 정성 데이터를 추가 보도에 활용했습니다.</p>",
+    "descriptionEng": "<p>A special news page project with the social team. With Radar-Chart graphic, we want to explain that every individual has their major and minor properties, which could be make them to be authorized but also to be discriminated by the society.</p><p>Users can make their own unique radar chart by participating simple survey on the page.</p>",
     "able": "true"
   },
   {
     "id": "4",
-    "slider-type": "0",
-    "title": "김용균",
+    "type": "0",
+    "title": "산업재해, 1748번의 죽음의 기록",
     "date": "2019.11",
     "thumb": "port-thumb-4.jpg",
-    "bigimage": null,
-    "link": null,
-    "workField": null,
-    "tag": null,
-    "description": null,
-    "descriptionEng": null,
+    "bigimage": "port_v_5_1.jpg, port_v_5_2.jpg, port_v_5_3.jpg, port_v_5_4.jpg, port_v_5_5.jpg",
+    "link": "http://news.khan.co.kr/kh_storytelling/2019/labordeath/index.html",
+    "workField": "페이지 디자인·기획(100%), 데이터 조사·가공(50%), 페이지 개발(100%), 페이지 내 기사 작성(30%)",
+    "tag": "HTML, CSS, Javascript, D3.js, leaflet.js, GoogleSpreadSheets",
+    "description": "<p>2018년부터 2019년까지 2년 동안 발생한 산업재해 사망사고를 전수 조사하여 살펴본 데이터 분석 기사입니다. 사망 사고 한 명, 한 명을의 기록을 담은 디지털 아카이브의 기획 및 제작을 담당했습니다.</p><p>한국산업안전보건공단이 작성한 재해조사 의견서를 입수해 이를 토대로 사망 사고 약 2천 건의 정보를 DB화하였습니다. 이 DB를 토대로 D3.js를 이용해 2천 건의 죽음을 사람의 형상으로 시각화했습니다.</p><p>데이터 정보값 추가: 사망사고의 50%가 추락사로 인한 사망이었고, 이를 사용자들이 한눈에 확인할 수 있도록 사고 원인 유형에 따라 사람 형상의 색상 및 형태를 다르게 표현했습니다.</p><p>필터링 기능: 사고의 원인 외에도 사망자에 대한 정보, 산업장 규모 등에 따라 분류하여 볼 수 있도록 했습니다.</p><p>캘린더 히트맵 그래픽으로 일별 사망인원의 분포도를 시각화했습니다. 그래픽을 통해 2년의 시간 동안 단 67일을 제외하고 매일 사고가 발생했음을, 하루에 10명이 넘는 사망자가 발생한 날도 있었음을 시사했습니다.</p><p>Google Map API를 활용해 사고가 발생한 위치의 위경도 값을 찾아내 이를 지도 위에 시각화했습니다.</p>",
+    "descriptionEng": "<p>A special news article page archiving thousands of death due to industrial disaster during 2018-2019. SVG graphic and line graph are drawn by D3.js & data mapping by leaflet.js</p>",
     "able": "true"
   },
   {
     "id": "5",
-    "slider-type": "0",
+    "type": "0",
     "title": "대기업브랜드지도",
     "date": "2019.01",
     "thumb": "port-thumb-5.jpg",
@@ -81,11 +81,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "6",
-    "slider-type": "0",
+    "type": "0",
     "title": "남북 70년",
     "date": "2018.04",
     "thumb": "port-thumb-6.jpg",
@@ -95,11 +95,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "7",
-    "slider-type": "0",
+    "type": "0",
     "title": "박전무 대 김대리",
     "date": "2018.04",
     "thumb": "port-thumb-7.jpg",
@@ -109,11 +109,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "8",
-    "slider-type": "0",
+    "type": "0",
     "title": "월급의 역사",
     "date": "2018.02",
     "thumb": "port-thumb-8.jpg",
@@ -123,11 +123,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "9",
-    "slider-type": "0",
+    "type": "0",
     "title": "고위직 ",
     "date": "2017.08",
     "thumb": "port-thumb-9.jpg",
@@ -137,11 +137,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "10",
-    "slider-type": "0",
+    "type": "0",
     "title": "내각구성",
     "date": "2017.08",
     "thumb": "port-thumb-10.jpg",
@@ -151,11 +151,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "11",
-    "slider-type": "0",
+    "type": "0",
     "title": "대한민국정당사",
     "date": "2017.01",
     "thumb": "port-thumb-28.jpg",
@@ -165,11 +165,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "12",
-    "slider-type": "1",
+    "type": "1",
     "title": "아파트",
     "date": "2019.10",
     "thumb": "port-thumb-12.jpg",
@@ -179,11 +179,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "13",
-    "slider-type": "1",
+    "type": "1",
     "title": "화학물질",
     "date": "2019.08",
     "thumb": "port-thumb-13.jpg",
@@ -193,11 +193,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "14",
-    "slider-type": "1",
+    "type": "1",
     "title": "X세대 리포트",
     "date": "2019.07",
     "thumb": "port-thumb-14.jpg",
@@ -207,11 +207,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "15",
-    "slider-type": "1",
+    "type": "1",
     "title": "그려진 노동",
     "date": "2019.05",
     "thumb": "port-thumb-15.jpg",
@@ -221,11 +221,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "16",
-    "slider-type": "1",
+    "type": "1",
     "title": "허스토리",
     "date": "2019.03",
     "thumb": "port-thumb-16.jpg",
@@ -235,11 +235,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "17",
-    "slider-type": "1",
+    "type": "1",
     "title": "로힝야",
     "date": "2019.02",
     "thumb": "port-thumb-17.jpg",
@@ -249,11 +249,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "18",
-    "slider-type": "1",
+    "type": "1",
     "title": "나어독",
     "date": "2019.02",
     "thumb": "port-thumb-18.jpg",
@@ -263,11 +263,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "19",
-    "slider-type": "1",
+    "type": "1",
     "title": "숨은사람찾기",
     "date": "2018.10",
     "thumb": "port-thumb-19.jpg",
@@ -277,11 +277,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "20",
-    "slider-type": "1",
+    "type": "1",
     "title": "랭면의취향",
     "date": "2018.07",
     "thumb": "port-thumb-20.jpg",
@@ -291,11 +291,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "21",
-    "slider-type": "1",
+    "type": "1",
     "title": "역대우리동네승자는",
     "date": "2018.06",
     "thumb": "port-thumb-21.jpg",
@@ -305,11 +305,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "22",
-    "slider-type": "1",
+    "type": "1",
     "title": "세상을 고발한여성들",
     "date": "2018.03",
     "thumb": "port-thumb-22.jpg",
@@ -319,11 +319,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "23",
-    "slider-type": "1",
+    "type": "1",
     "title": "전교1등",
     "date": "2018.01",
     "thumb": "port-thumb-23.jpg",
@@ -333,11 +333,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "24",
-    "slider-type": "1",
+    "type": "1",
     "title": "후보정책한눈에",
     "date": "2017.04",
     "thumb": "port-thumb-25.jpg",
@@ -347,11 +347,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "25",
-    "slider-type": "1",
+    "type": "1",
     "title": "대선주자 10장면",
     "date": "2017.04",
     "thumb": "port-thumb-26.jpg",
@@ -361,11 +361,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "26",
-    "slider-type": "2",
+    "type": "2",
     "title": "반지하 상단 그래픽",
     "date": "2020.04",
     "thumb": "port-thumb-26.jpg",
@@ -375,11 +375,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "27",
-    "slider-type": "2",
+    "type": "2",
     "title": "멀티캠퍼스 내부 그래픽",
     "date": "2020.03",
     "thumb": "port-thumb-26.jpg",
@@ -389,11 +389,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "28",
-    "slider-type": "2",
+    "type": "2",
     "title": "정당사",
     "date": "2017.01",
     "thumb": "port-thumb-26.jpg",
@@ -403,11 +403,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "29",
-    "slider-type": "2",
+    "type": "2",
     "title": "읽씹뉴스",
     "date": "2019.03",
     "thumb": "port-thumb-26.jpg",
@@ -417,11 +417,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "30",
-    "slider-type": "3",
+    "type": "3",
     "title": "이런경향",
     "date": "2019.03",
     "thumb": "port-thumb-26.jpg",
@@ -431,11 +431,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "31",
-    "slider-type": "3",
+    "type": "3",
     "title": "뉴스밍",
     "date": "2020.04",
     "thumb": "port-thumb-26.jpg",
@@ -445,11 +445,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "32",
-    "slider-type": "3",
+    "type": "3",
     "title": "경향신문 공유",
     "date": "2018.02",
     "thumb": "port-thumb-26.jpg",
@@ -459,11 +459,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "33",
-    "slider-type": "4",
+    "type": "4",
     "title": "카드뉴스1",
     "date": "2017",
     "thumb": "port-thumb-26.jpg",
@@ -473,11 +473,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "34",
-    "slider-type": "4",
+    "type": "4",
     "title": "카드뉴스1",
     "date": "2017",
     "thumb": "port-thumb-26.jpg",
@@ -487,11 +487,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "35",
-    "slider-type": "5",
+    "type": "5",
     "title": "영상1",
     "date": "2018",
     "thumb": "port-thumb-26.jpg",
@@ -501,11 +501,11 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   },
   {
     "id": "36",
-    "slider-type": "5",
+    "type": "5",
     "title": "영상2",
     "date": "2018",
     "thumb": "port-thumb-26.jpg",
@@ -515,6 +515,6 @@ var portItemList = [
     "tag": null,
     "description": null,
     "descriptionEng": null,
-    "able": "true"
+    "able": "FASLE"
   }
 ];
